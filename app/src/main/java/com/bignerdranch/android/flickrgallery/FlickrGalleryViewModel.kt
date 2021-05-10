feb219607e1abf6ea32be12a8c96a106bc43a168
Michael Lu <yeskky@gmail.com>
2021-05-10 20:02:03 +0800

    显示图片标题

---
+++ app/src/main/java/com/bignerdranch/android/flickrgallery/FlickrGalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.flickrgallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.flickrgallery.api.GalleryItem
+
+class FlickrGalleryViewModel : ViewModel() {
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+    }
+}
